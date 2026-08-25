f50df543274b3e9f9a99dd77e3dca45539f65695
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-08-25 07:30:15 +0530

    Array Defining

---
+++ Java/Arrays/Defining.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Defining {
+    public static void main(String[] args) {
+        
+        int[] marks = new int[3];
+        //int marks[] = new int[3];
+        marks[0] = 80;
+        marks[1] = 91;
+        marks[2] = 98;
+        System.out.println(marks); //this prints Garbage - Jargon Value
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for(int i=0; i<3; i++){
+            System.out.print(marks[i] + " ");
+        }
+    }
+}
