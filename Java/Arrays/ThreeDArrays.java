6d9ce10d17e044a4efd8b95c55e6f8b6c8b54080
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-04 21:59:34 +0530

    3D Arrays - Predefined

---
+++ Java/Arrays/ThreeDArrays.java
@@ -0,0 +1,18 @@
+package Arrays;
+public class ThreeDArrays {
+    public static void main(String[] args) {
+
+        int[][][] arr = {
+            {
+                {1, 2, 3},
+                {4, 5, 6}
+            },
+            {
+                {7, 8, 9},
+                {10, 11, 12}
+            }
+        };
+
+        System.out.println(arr[0][1][2]);
+    }
+}
