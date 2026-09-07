95e8fe54dee299cdf4752f3346f3efd005e3ad82
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-07 20:59:11 +0530

    NPTEL - Assignment -2 6th One

---
+++ NPTEL_JAVA/NPTEL_W2.java
@@ -0,0 +1,19 @@
+package NPTEL_JAVA;
+
+class NPTEL_W2 {
+
+    int x;
+
+    NPTEL_W2(int x) {
+        this.x = x;
+    }
+
+    void printX() {
+        System.out.println(this.x);
+    }
+
+    public static void main(String[] args) {
+        NPTEL_W2 obj = new NPTEL_W2(000);
+        obj.printX();
+    }
+}
