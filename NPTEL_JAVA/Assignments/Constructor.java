b8c3d5cc59ba55021ae9a89afec87e6d3f377f92
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-09 20:39:32 +0530

    NPTEL - Assignment -3 1st Constructor Rules

---
+++ NPTEL_JAVA/Assignments/Constructor.java
@@ -0,0 +1,19 @@
+package NPTEL_JAVA.Assignments;
+
+class Animal {
+    Animal() {
+        System.out.println("Animal constructor");
+    }
+}
+
+class Dog extends Animal {
+    Dog() {
+        System.out.println("Dog constructor");
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+    }
+}
